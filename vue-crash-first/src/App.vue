241026de19b6b3b0c40f7e482a174f88a9620cb1
Mohamadou Boly <mohamedbolydev@gmail.com>
2025-08-02 08:59:52 -0400

    vue if et vue for

---
+++ vue-crash-first/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+   <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'inactivedeedtive', 
+      tasks: ['task1', 'task2', 'task3'],
+    };
+  },
+};
+</script>
+
+
